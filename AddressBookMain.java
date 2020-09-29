93a1a663ca15fb9364742e851873fd2f4a167748
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 17:10:16 +0530

    UC2 Commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,17 +4,50 @@ public class AddressBookMain
 {
     public static void main(String[] args)
     {
-        Contact contact1=new Contact("Omkeshwari","Pilare","Matunga","Mumbai","Maharashtra","400022","987654321","omp@gmail.com");
+        ArrayList<Contact> contactList=new ArrayList<Contact>();
+        Scanner sc=new Scanner(System.in);
+
+        String firstName, lastName, address, city, state, zip, phoneNumber, email;
+
+        System.out.println("Enter First Name:");
+        firstName=sc.next();
+
+        System.out.println("Enter Last Name:");
+        lastName=sc.next();
         
+        System.out.println("Enter Address:");
+        address=sc.next();
+
+        System.out.println("Enter City:");
+        city=sc.next();
+
+        System.out.println("Enter State:");
+        state=sc.next();
+
+        System.out.println("Enter Zip:");
+        zip=sc.next();
+
+        System.out.println("Enter Phone Number:");
+        phoneNumber=sc.next();
+
+        System.out.println("Enter Email:");
+        email=sc.next();
+
+        Contact contact1=new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
+        contactList.add(contact1);
+        
+        for(Contact lobj:contactList)
+        {   
             System.out.println("--------------------------------");
-            System.out.println("First Name: "+contact1.getFirstName());
-            System.out.println("Last Name: "+contact1.getLastName());
-            System.out.println("Address: "+contact1.getAddress());
-            System.out.println("City: "+contact1.getCity());
-            System.out.println("State: "+contact1.getState());
-            System.out.println("Zip: "+contact1.getZip());
-            System.out.println("Phone Number: "+contact1.getPhoneNumber());
-            System.out.println("Email: "+contact1.getEmail());
+            System.out.println("First Name: "+lobj.getFirstName());
+            System.out.println("Last Name: "+lobj.getLastName());
+            System.out.println("Address: "+lobj.getAddress());
+            System.out.println("City: "+lobj.getCity());
+            System.out.println("State: "+lobj.getState());
+            System.out.println("Zip: "+lobj.getZip());
+            System.out.println("Phone Number: "+lobj.getPhoneNumber());
+            System.out.println("Email: "+lobj.getEmail());
             System.out.println("--------------------------------");
+        }
     }
 }
