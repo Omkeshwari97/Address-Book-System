441f18e997e95d02b44e0ffad48a78b61160b046
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-11 02:31:49 +0530

    Commit: View Person details as per city or state

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -61,6 +61,56 @@ public class AddressBookMain
 		}	
 	}
 	
+	void viewPersonsByCity(String city)
+	{
+		System.out.println("City : " + city);
+						
+		for(Map.Entry<String, AddressBook> entry : addressBookNameMap.entrySet())
+		{					
+			for(Contact listContact : entry.getValue().getContacts())
+			{
+				if(listContact.getCity().equals(city))
+				{
+					System.out.println("--------------------------------");
+	                System.out.println("First Name: "+listContact.getFirstName());
+	                System.out.println("Last Name: "+listContact.getLastName());
+	                System.out.println("Address: "+listContact.getAddress());
+	                System.out.println("City: "+listContact.getCity());
+	                System.out.println("State: "+listContact.getState());
+	                System.out.println("Zip: "+listContact.getZip());
+	                System.out.println("Phone Number: "+listContact.getPhoneNumber());
+	                System.out.println("Email: "+listContact.getEmail());
+	                System.out.println("--------------------------------");			
+				}
+			}
+		}	
+	}
+	
+	void viewPersonsByState(String state)
+	{
+		System.out.println("State : " + state);
+						
+		for(Map.Entry<String, AddressBook> entry : addressBookNameMap.entrySet())
+		{					
+			for(Contact listContact : entry.getValue().getContacts())
+			{
+				if(listContact.getState().equals(state))
+				{
+					System.out.println("--------------------------------");
+	                System.out.println("First Name: "+listContact.getFirstName());
+	                System.out.println("Last Name: "+listContact.getLastName());
+	                System.out.println("Address: "+listContact.getAddress());
+	                System.out.println("City: "+listContact.getCity());
+	                System.out.println("State: "+listContact.getState());
+	                System.out.println("Zip: "+listContact.getZip());
+	                System.out.println("Phone Number: "+listContact.getPhoneNumber());
+	                System.out.println("Email: "+listContact.getEmail());
+	                System.out.println("--------------------------------");			
+				}
+			}
+		}	
+	}
+	
 	
     public static void main(String[] args) 
     {    
@@ -103,17 +153,25 @@ public class AddressBookMain
             }
         }while(choice>=0 && choice<=4);
     
-        System.out.println("Enter city to view person:");
+        System.out.println("Enter city to search person:");
         String cityString = scanner.next();
         System.out.println("Enter first name:");
         String personNameString = scanner.next();
         objAddressBookMain.searchPersonByCity(cityString, personNameString);
         
 
-        System.out.println("Enter state to view persons:");
+        System.out.println("Enter state to search person:");
         String stateString = scanner.next();
         System.out.println("Enter first name:");
         String nameString = scanner.next();
         objAddressBookMain.searchPersonByState(stateString,nameString);
+        
+        System.out.println("Enter city to view persons:");
+        String cityNameString = scanner.next();
+        objAddressBookMain.viewPersonsByCity(cityNameString);
+        
+        System.out.println("Enter state to view persons:");
+        String stateNameString = scanner.next();
+        objAddressBookMain.viewPersonsByState(stateNameString);
     }
 }
